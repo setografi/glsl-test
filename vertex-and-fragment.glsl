09f7cec330d5b1d346261e736297a3fecf7b7c6b
setografi <hitonaashbell@gmail.com>
2024-04-08 23:25:44 +0700

    glsl test

---
+++ vertex-and-fragment.glsl
@@ -0,0 +1,43 @@
+// Vertex shader
+attribute vec2 a_position;
+void main() {
+    gl_Position = vec4(a_position, 0.0, 1.0);
+}
+
+// Fragment shader
+precision mediump float;
+
+uniform vec2 u_resolution;
+uniform float u_time;
+
+vec3 palette(float t) {
+    vec3 a = vec3(0.5, 0.5, 0.5);
+    vec3 b = vec3(0.5, 0.5, 0.5);
+    vec3 c = vec3(1.0, 1.0, 1.0);
+    vec3 d = vec3(0.263, 0.416, 0.557);
+
+    return a + b * cos(6.28318 * (c * t + d));
+}
+
+void mainImage(out vec4 fragColor, in vec2 fragCoord) {
+    vec2 uv = (fragCoord * 2.0 - u_resolution) / u_resolution.y;
+    vec2 uv0 = uv;
+    vec3 finalColor = vec3(0.0);
+
+    for (float i = 0.0; i < 4.0; i++) {
+        uv = fract(uv * 1.5) - 0.5;
+
+        float d = length(uv) * exp(-length(uv0));
+
+        vec3 col = palette(length(uv0) + i * 0.4 + u_time * 0.4);
+
+        d = sin(d * 8.0 + u_time) / 8.0;
+        d = abs(d);
+
+        d = pow(0.01 / d, 1.2);
+
+        finalColor += col * d;
+    }
+
+    fragColor = vec4(finalColor, 1.0);
+}
